cb9a578b9a877de680bccc3cdc89a243fec58b0f
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2023-10-19 16:53:41 +0100

    Simple server endpoint

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/log", async (req, res) => {
+  res.send({
+    event: {
+      transaction_id: 1,
+      data_set: "my-logging-app"
+    },
+    message: "WARN: Unable to get an interesting response"
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
